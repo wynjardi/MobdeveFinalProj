1b689916674d1a7f99029b6805e170165859aef5
MitziSee07 <70153916+MitziSee07@users.noreply.github.com>
2022-02-13 01:16:29 +0800

    has admind and user activity with dashboards pwede change

---
+++ app/src/main/java/com/mobdeve/jardiniano/see/DashboardUserActivity.kt
@@ -0,0 +1,42 @@
+package com.mobdeve.jardiniano.see
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil.setContentView
+import com.google.firebase.auth.FirebaseAuth
+
+import com.mobdeve.jardiniano.see.databinding.ActivityDashboardUserBinding
+
+
+class DashboardUserActivity : AppCompatActivity(){
+
+
+    private lateinit var binding: ActivityDashboardUserBinding
+
+    //firebase
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDashboardUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //init firebase
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+
+
+    }
+
+    private fun checkUser(){
+        //get user
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null){
+            val email = firebaseUser.email
+            binding.TitleTv.text=email
+        }
+
+
+    }
+}
--- app/src/main/java/com/mobdeve/jardiniano/see/Login.kt
+++ app/src/main/java/com/mobdeve/jardiniano/see/Login.kt
@@ -13,6 +13,10 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import com.mobdeve.jardiniano.see.databinding.ActivityLoginBinding
 
@@ -90,6 +94,8 @@ class Login : AppCompatActivity() {
         progressDialog.show()
         firebaseAuth.signInWithEmailAndPassword(email, password)
             .addOnSuccessListener {
+
+                checkUser()
                 //login success
                 progressDialog.dismiss()
                 //get user info
@@ -111,11 +117,51 @@ class Login : AppCompatActivity() {
 
     private fun checkUser() {
         //if user is already logged in, redirect to profile activity
-        val firebaseUser = firebaseAuth.currentUser
-        if (firebaseUser != null){
-            //user is logged in
-            startActivity(Intent(this, ProfileActivity::class.java))
-            finish()
-        }
+        val firebaseUser = firebaseAuth.currentUser!!
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseUser.uid)
+            .addListenerForSingleValueEvent(object: ValueEventListener{
+
+                override fun onDataChange(snapshot: DataSnapshot){
+                    progressDialog.dismiss()
+                    val userType = snapshot.child("userType").value
+                    if (userType == "user"){
+                        startActivity(Intent(this@Login, DashboardUserActivity::class.java))
+                            finish()
+                    }
+                    else if (userType == "admin"){
+                        startActivity(Intent(this@Login, DashboardAdminActivity::class.java))
+                        finish()
+                    }
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+//        if (firebaseUser != null){
+//            //user is logged in
+//            startActivity(Intent(this, ProfileActivity::class.java))
+//            finish()
+//        }
     }
+
+//    private fun loginUser(){
+//
+//        progressDialog.setMessage("Logging in")
+//        progressDialog.show()
+//
+//        firebaseAuth.signInWithEmailAndPassword(email, password)
+//            .addOnSuccessListener {
+//
+//                checkUser()
+//            }
+//
+//            .addOnFailureListener { e->
+//                Toast.makeText(this,"Logged in failed", Toast.LENGTH_SHORT).show()
+//            }
+//    }
 }
