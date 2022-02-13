99b1962f124b8c6a438b081256edafd3df904770
wynjardi <88533780+wynjardi@users.noreply.github.com>
2022-02-13 23:32:30 +0800

    codes for activity concert details updated

---
+++ app/src/main/java/com/mobdeve/jardiniano/see/ConcertDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.jardiniano.see
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConcertDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_concert_detail)
+    }
+}
--- app/src/main/java/com/mobdeve/jardiniano/see/ConcertDetailsActivity.kt
+++
@@ -1,4 +0,0 @@
-package com.mobdeve.jardiniano.see
-
-class ConcertDetailsActivity {
-}
