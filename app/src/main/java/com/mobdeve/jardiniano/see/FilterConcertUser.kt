36b05f1257a2aba343eb17a4f74778fdaaa07270
wynjardi <88533780+wynjardi@users.noreply.github.com>
2022-02-15 03:32:55 +0800

    Update FilterConcertUser.kt

--- app/src/main/java/com/mobdeve/jardiniano/see/FilterConcertUser.kt
+++ app/src/main/java/com/mobdeve/jardiniano/see/FilterConcertUser.kt
@@ -16,12 +16,40 @@ class FilterConcertUser: Filter {
         this.adapterConcertUser = adapterConcertUser
     }
 
-    override fun performFiltering(p0: CharSequence?): FilterResults {
+    override fun performFiltering(constraint: CharSequence): FilterResults {
 
+        var constraint: CharSequence? = constraint
+        val results = FilterResults()
+
+        if (constraint != null && constraint.isNotEmpty()){
+            //not null or empty
+
+            constraint = constraint.toString().uppercase()
+            val filteredModels = ArrayList<ModelConcert>()
+            for (i in filterList.indices){
+                //validate if match
+                if (filterList[i].concertName.uppercase().contains(constraint)){
+                    //searched value matched with concert name, add to list
+                    filteredModels.add(filterList[i])
+                }
+            }
+            //return filtered list
+            results.values = filteredModels
+        }
+        else{
+            //either null or empty, return og list
+            results.values = filterList
+        }
+
+        return results
     }
 
-    override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+    override fun publishResults(constraint: CharSequence, results: FilterResults) {
+        //apply filter changes
+        adapterConcertUser.concertArrayList = results.values as ArrayList<ModelConcert>
 
+        //notify changes
+        adapterConcertUser.notifyDataSetChanged()
     }
 
 
