c74148c2fa1281fccddb7d8e690dcfea7d08bc8b
MitziSee07 <70153916+MitziSee07@users.noreply.github.com>
2022-02-14 22:12:27 +0800

    createdf myApp and updated ConcertAdminAdapter

--- app/src/main/java/com/mobdeve/jardiniano/see/MyApplication.kt
+++ app/src/main/java/com/mobdeve/jardiniano/see/MyApplication.kt
@@ -5,6 +5,11 @@ import android.text.format.DateFormat
 import android.util.Log
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.TextView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.storage.FirebaseStorage
 import com.google.firebase.storage.ktx.storageMetadata
 import java.sql.Timestamp
@@ -48,6 +53,25 @@ class MyApplication: Application() {
 
         }
 
+        fun loadCategory (categoryId: String, categoryTv: TextView){
+            val ref = FirebaseDatabase.getInstance().getReference("Categories")
+            ref.child(categoryId)
+                .addListenerForSingleValueEvent(object: ValueEventListener{
+                    override fun onDataChange(snapshot: DataSnapshot) {
+
+                        val category= "${snapshot.child("category").value}"
+
+                        //set categ
+                        categoryTv.text = category
+                    }
+
+                    override fun onCancelled(error: DatabaseError) {
+
+                    }
+                })
+        }
+
     }
+
 
 }
